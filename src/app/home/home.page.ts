abdcf6b4cf5523a34c7164a428ee5c94f349b6d4
Mohamad Tawakol <tawakol@paywithspare.com>
2024-07-18 16:55:21 +0300

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
+  providers: [InAppBrowser]
+})
+export class HomePage {
+  constructor(private inAppBrowser: InAppBrowser) {}
+
+  launchSpareApp() {
+    this.inAppBrowser.create('https://spare-parent-rcph76b7h-spare.vercel.app/auth/welcome?vercelToolbarCode=Z3slT8Zrg22Pxle', '_blank');
+  }
+}
